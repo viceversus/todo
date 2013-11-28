d7c0adcaa586be41ba4d2775332b7bd2be7e197e
Ken Shimizu <kenshimizu88@gmail.com>
2013-11-27 16:28:43 -0800

    Initial Commit

---
+++ todo/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  todo
+//
+//  Created by Ken Shimizu on 11/27/13.
+//  Copyright (c) 2013 Ken Shimizu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
